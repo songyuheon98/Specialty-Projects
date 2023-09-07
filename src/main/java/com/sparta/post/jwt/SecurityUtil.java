deb9a3a2c5e7b5111ca1e943fc583ee453b6db29
iksadNorth <iksadNorth@gmail.com>
2023-09-07 12:30:12 +0900

    Principal Getter 구현

---
+++ src/main/java/com/sparta/post/jwt/SecurityUtil.java
@@ -0,0 +1,20 @@
+package com.sparta.post.jwt;
+
+import com.sparta.post.entity.User;
+import com.sparta.post.security.UserDetailsImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SecurityUtil {
+    public static Optional<User> getPrincipal() {
+        return Optional.of(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(UserDetailsImpl.class::cast)
+                .map(UserDetailsImpl::getUser);
+    }
+}
